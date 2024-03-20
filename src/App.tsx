e5e46fdd7a591bb0af8de74e66a57b061c4b4a7d
Tom Hartman <thomas.lees.hartman@gmail.com>
2024-03-20 13:16:25 -0400

    Modals
    Added simple modal ui component with basic functionality and
    styling. Added a helpModal with some basic command information for
    grid keys.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import './App.css';
 import GridLayout from './components/layouts/gridLayout';
 import GridSummary from './components/ui/gridSummary';
 import gridReducer from './state/gridContext';
 import { initGrid } from './utils/gridUtilities.ts';
 import ClueLayout from './components/layouts/clueLayout.tsx';
+import HelpModal from './components/layouts/helpModal.tsx';
 
 const defaultGridSize = 15;
 
 function App() {
   const [grid, dispatch] = useReducer(gridReducer, initGrid(defaultGridSize,
                                                             defaultGridSize));
+
+  const [openHelpModal, setOpenHelpModal] = useState(false);
+
+  const onHelpButtonClickHandler = () => {
+    setOpenHelpModal(!openHelpModal);
+  };
+
   return (
     <>
       <div className='header'>CrissCross
         <div className='header-icons'>
           <button>&#10227;</button>
-          <button>?</button>
+          <button onClick={onHelpButtonClickHandler}>?</button>
         </div>
       </div>
       <div className='main'>
@@ -26,6 +34,7 @@ function App() {
       <div className='clues'>
         <ClueLayout grid={grid} />
       </div>
+      <HelpModal isOpen={openHelpModal} setIsOpen={setOpenHelpModal}/>
     </>
   );
 }
