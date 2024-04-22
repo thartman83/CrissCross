8f08fb194bf2a133aad207af39f12d589a375d36
Tom Hartman <thomas.lees.hartman@gmail.com>
2024-04-22 18:35:47 -0400

    Path aliases
    Added path aliases for @/ to make code imports a bit more readable and flexible.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    testMatch: ['./src/tests/**/*.test.tsx'],
+    globals: true,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
+});
